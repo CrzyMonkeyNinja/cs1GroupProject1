bec4ebc9429050766baed9e8ef1103bd1388eeae
Matthew Boustany <mb6gm@virginia.edu>
2015-11-18 12:14:27 -0500

    Changed comments from reflecting a cat based simulation.

--- dummies.py
+++ dummies.py
@@ -5,28 +5,18 @@ from random import randint
 
 ################################################################
 
-# This program is an interactive simulation/game. A cat starts
-# to move across the screen. The direction of movement is reversed
-# on each "mouse down" event.
+# This program is an interactive simulation/game. Two characters move
+# across the screen (controlled by WASD and IJKL for a multiplayer
+# game) and can slap each other backwards with another key command.
 #
-# The state of the cat is represented by a tuple (pos, delta-pos).
-# The first element, pos, represents the x-coordinate of the cat.
-# The second element, delta-pos, represents the amount that the
-# position changes on each iteration of the simulation loop.
+# The state of the torso is represented by a tuple (x position, y
+# position, state of arm, for both players).
 #
-# For example, the tuple (7,1) would represent the cat at x-coord,
-# 7, and moving to the right by 1 pixel per "clock tick."
+# A slap initiated outside of a certain region will have no affect on
+# the other character's location, but the arm will still swing.
 #
-# The initial state of the cat in this program is (0,1), meaning that the cat
-# starts at the left of the screen and moves right one pixel per tick.
-#
-# Pressing a mouse button down while this simulation run updates the cat state
-# by leaving pos unchanged but reversing delta-pos (changing 1 to -1 and vice
-# versa). That is, pressing a mouse key reverses the direction of the
-# cat.
-#
-# The simulation ends when the cat is allowed to reach either the left
-# or the right edge of the screen.
+# The simulation ends when either player leaves the "ring" by fleeing
+# or by being pushed out by the other player's slaps. 
 
 ################################################################
 
@@ -38,7 +28,7 @@ rw.newDisplay(width, height, name)
 
 ################################################################
 
-# Display the state by drawing a cat at that x coordinate
+# Display the state by drawing two torsos with arms.
 rightTorso = dw.loadImage("T_R.png")
 rightHigh = dw.loadImage("R_H.png")  # -150, +255
 rightLow = dw.loadImage("R_L.png")  # -150, -100
@@ -46,11 +36,6 @@ rightLow = dw.loadImage("R_L.png")  # -150, -100
 leftTorso = dw.loadImage("T_L.png")
 leftHigh = dw.loadImage("L_H.png")  # 200, +225
 leftLow = dw.loadImage("L_L.png")  # 200, -100
-
-
-# state -> image (IO)
-# draw the cat halfway up the screen (height/2) and at the x
-# coordinate given by the first component of the state tuple
 
 
 def updateDisplay(state):
@@ -70,12 +55,6 @@ def updateDisplay(state):
 
 
 ################################################################
-
-# Change pos by delta-pos, leaving delta-pos unchanged
-# Note that pos is accessed as state[0], and delta-pos
-# as state[1]. Later on we'll see how to access state
-# components by name (as we saw with records in Idris).
-#
 # state -> state
 def updateState(state):
     print("frame", state[7])
@@ -85,7 +64,7 @@ def updateState(state):
 
 # Terminate the simulation when the x coord reaches the screen edge,
 # that is, when pos is less then zero or greater than the screen width
-# state -> bool
+# state -> bool, or when y coord reaches screen edge for either image.
 
 
 def endState(state):
@@ -112,15 +91,6 @@ def endState(state):
 
 ################################################################
 
-# We handle each event by printing (a serialized version of) it on the console
-# and by then responding to the event. If the event is not a "mouse button down
-# event" we ignore it by just returning the current state unchanged. Otherwise
-# we return a new state, with pos the same as in the original state, but
-# delta-pos reversed: if the cat was moving right, we update delta-pos so that
-# it moves left, and vice versa. Each mouse down event changes the cat
-# direction. The game is to keep the cat alive by not letting it run off the
-# edge of the screen.
-#
 # state -> event -> state
 #
 def handleEvent(state, event):
@@ -170,9 +140,6 @@ def handleEvent(state, event):
 
 ################################################################
 
-# World state will be single x coordinate at left edge of world
-
-# The cat starts at the left, moving right
 initState = (100, 175, 900, 175, randint(0, 1), randint(0, 1), 120, 120)
 
 # Run the simulation no faster than 60 frames per second
